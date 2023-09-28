656753ccf89b0890f183b1c211ba92bfd6aad0e6
Pierre Bouchet <pierre.bouchet@brightloop.fr>
2023-09-28 15:39:15 +0200

    Update filename argument placeholders

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from logexport import *
 
 
 def export_from_blf():
-    source_file = '<relevant BLF data file>'
+    source_file = '../data/<relevant BLF data filename>.blf'
     print('> SHA256 of log file: {}'.format(get_sha(source_file)))
     reader = can.BLFReader(source_file)
-    dbc_file = '<relevant DBC file>'
-    dbc_filter = DbcFilter(partly_accepted=DATA_SOURCES_EXAMPLE)
+    dbc_file = '../dbc/<relevant DBC filename>.dbc'
+    dbc_filter = DbcFilter(accept_all=True)
 
-    export = LogExport(dbc_file, dbc_filter, dchv_shortname,
-                       expected_frame_count=reader.object_count)
+    export = LogExport(dbc_file, dbc_filter,
+                       signal_renamer=dchv_shortname,
+                       expected_frame_count=reader.object_count,
+                       use_sample_and_hold=False)
     for frame in reader:
         export.process_frame(frame)
 
@@ -20,13 +22,15 @@ def export_from_blf():
 
 
 def export_from_trc():
-    source_file = '<relevant TRC data file>'
+    source_file = '../data/<relevant TRC data filename>.trc'
     print('> SHA256 of log file: {}'.format(get_sha(source_file)))
     reader = can.TRCReader(source_file)
-    dbc_file = '<relevant DBC file>'
+    dbc_file = '../dbc/<relevant DBC filename>.dbc'
     dbc_filter = DbcFilter(accept_all=True)
 
-    export = LogExport(dbc_file, dbc_filter, target_channel=1,
+    export = LogExport(dbc_file, dbc_filter,
+                       signal_renamer=dchv_shortname,
+                       target_channel=1,
                        expected_frame_count=count_lines(source_file))
     for frame in reader:
         export.process_frame(frame, allow_truncated=True)
@@ -36,4 +40,4 @@ def export_from_trc():
 
 
 if __name__ == '__main__':
-    export_from_trc()
+    export_from_blf()
