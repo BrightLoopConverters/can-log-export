92af2796c1338152e6b86d6763ed2ad507e67a6a
Pierre Bouchet <pierre.bouchet@brightloop.fr>
2023-11-03 16:27:53 +0100

    Create export_from_asc() function

--- logexport/main.py
+++ logexport/main.py
@@ -32,6 +32,27 @@ def export_from_trc():
                        signal_renamer=dchv_shortname,
                        target_channel=1,
                        expected_frame_count=count_lines(source_file))
+
+    for frame in reader:
+        export.process_frame(frame, allow_truncated=True)
+
+    export.print_info()
+    export.write_csv(source_file)
+
+
+def export_from_asc():
+    source_file = '../data/<relevant ASC data filename>.asc'
+    print('> SHA256 of log file: {}'.format(get_sha(source_file)))
+    reader = can.ASCReader(source_file)
+
+    dbc_file = '../dbc/<relevant DBC filename>.dbc'
+    dbc_filter = DbcFilter(accept_all=True)
+
+    export = LogExport(dbc_file, dbc_filter,
+                       signal_renamer=dchv_shortname,
+                       target_channel=0,
+                       expected_frame_count=count_lines(source_file))
+
     for frame in reader:
         export.process_frame(frame, allow_truncated=True)
 
@@ -40,4 +61,4 @@ def export_from_trc():
 
 
 if __name__ == '__main__':
-    export_from_blf()
+    export_from_asc()
