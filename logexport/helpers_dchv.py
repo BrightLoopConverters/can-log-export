0d45215920961ac525145f0e4e508d26b76b0be0
Pierre Bouchet <pierre.bouchet@brightloop.fr>
2023-11-13 10:25:29 +0100

    Remove data source filter example in helper module

--- logexport/helpers_dchv.py
+++ logexport/helpers_dchv.py
@@ -1,16 +1,3 @@
-DATA_SOURCES_EXAMPLE = {
-    'MESSAGE_1': [
-        'PARAM_1_x',
-        'PARAM_1_y',
-        'PARAM_1_z'
-    ],
-    'MESSAGE_2': [
-        'PARAM_2_x',
-        'PARAM_2_y',
-        'PARAM_2_z'
-    ]
-}
-
 def dchv_shortname(message_name, signal_name):
     message_name = message_name.removeprefix('Dcdc')
     return '{}.{}'.format(message_name, signal_name)
