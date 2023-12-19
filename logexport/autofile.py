d1465c70908ab1bbb5fc980745827123ee58969b
Pierre Bouchet <pierre.bouchet@brightloop.fr>
2023-12-19 23:19:09 +0100

    Parameterize possible_files() function

--- logexport/autofile.py
+++ logexport/autofile.py
@@ -4,29 +4,33 @@ from pathlib import Path
 
 
 def guess_data_file(dirpath):
-    files = possible_data_files(dirpath)
-    files = sorted(files, key=os.path.getmtime, reverse=True)
-    data_file = files[0]
+    files = possible_files(dirpath, is_possible_data_file)
+    files = sorted(files, key=os.path.getctime, reverse=True)
+    result = files[0]
 
     print(f'> Selecting data file automatically: found {len(files)} candidates in {dirpath}')
-    timestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_file.stat().st_mtime))
-    print(f'> Most recent data file: {data_file.name} (last modified: {timestr})')
-    return data_file
+    timestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result.stat().st_ctime))
+    print(f'> Most recent data file: {result.name} (created: {timestr})')
+    return result
 
-def possible_data_files(dirpath):
+def possible_files(dirpath, possible_file_test):
     paths = Path(dirpath).iterdir()
     files = []
     dirs = []
     for p in paths:
         if p.is_dir():
             dirs.append(p)
-        # Retain files that are neither zip nor .gitignore
-        elif p.is_file() and p.suffix != '.zip' and p.suffix != '.csv' and not p.stem.startswith('.'):
+        elif possible_file_test(p):
             files.append(p)
 
     # Recursion
     for d in dirs:
-        nested_files = possible_data_files(d)
+        nested_files = possible_files(d, possible_file_test)
         files.extend(nested_files)
 
     return files
+
+
+def is_possible_data_file(p):
+    # Retain files that are neither zip nor .gitignore
+    return p.is_file() and p.suffix != '.zip' and p.suffix != '.csv' and not p.stem.startswith('.')
